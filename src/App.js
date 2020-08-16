750528a0428cee8ca9a6c464ef543031148964c8
MohGumaa <gomaam14@gmail.com>
2020-08-16 19:42:07 +0300

    Add secure page with login redirect

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Route, withRouter } from "react-router-dom";
+import { Security, SecureRoute, LoginCallback } from "@okta/okta-react";
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import Staff from "./components/pages/Staff";
+import Login from "./components/auth/Login";
 
-function App() {
+function App(props) {
+  console.log(props);
+  const onAuthRequired = () => {
+    props.history.push("/login");
+  };
+
   return (
-    <Router>
+    <Security
+      issuer="https://dev-623026.okta.com/oauth2/default"
+      clientId="0oaqa0rrjKYjsMLRL4x6"
+      redirectUri={window.location.origin + "/implicit/callback"}
+      onAuthRequired={onAuthRequired}
+    >
       <div>
         <Navbar />
         <div className="container">
           <Route exact={true} path="/" component={Home} />
-          <Route exact={true} path="/staff" component={Staff} />
+          <SecureRoute exact={true} path="/staff" component={Staff} />
+          <Route
+            path="/login"
+            render={() => (
+              <Login baseUrl="https://dev-623026.okta.com/oauth2/default" />
+            )}
+          />
+          <Route path="/implicit/callback" component={LoginCallback} />
         </div>
       </div>
-    </Router>
+    </Security>
   );
 }
 
-export default App;
+export default withRouter(App);
